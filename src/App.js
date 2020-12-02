57e760d966bdc91189c4eb48ad894852e3b95c6b
darlancosta <darlandsv@gmail.com>
2020-12-02 19:38:23 -0300

    Tests ok!

--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    handleListRepository()
+  },[]);
+
+  async function handleListRepository() {
+    api.get('repositories').then(response => {
+      setProjects(response.data);
+    })
+  }
+
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories',{      
+      title: 'O projeto foda',
+      url: 'https://github.com/DarlanCosta/happy-children-mobile.git',
+      techs: ['JAVA', 'Javascript', 'Delphi'],
+    });
+
+    setProjects([...projects, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    await api.delete(`repositories/${id}`)
+
+    setProjects(projects.filter(project => project.id !== id ));
+
   }
 
   return (
     <div>
+        
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {projects.map((repository) => (
+          <li key={repository.id}>
+            
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
-          </button>
-        </li>
+            </button> 
+        
+          </li>
+          ))
+        }
       </ul>
+           
+          
+        
+         
+
+      
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
